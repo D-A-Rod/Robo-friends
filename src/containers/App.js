2c8da2ba51cf77817c4a2087745c689c7f621571
Daniel Rodriguez <drodrigo95@hotmail.com>
2023-05-08 10:39:06 -0500

    refactored a little

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import Scroll from './Scroll'
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "..components/Scroll";
 //import { robots } from "./robots";
 
 class App extends Component {
@@ -14,10 +14,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({robots: users}))
-
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -25,15 +24,16 @@ class App extends Component {
   };
 
   render() {
-    const filterRobots = this.state.robots.filter((robots) => {
+    const { robots, searchfield } = this.state;
+    const filterRobots = robots.filter((robots) => {
       return robots.name
         .toLocaleLowerCase()
-        .includes(this.state.searchfield.toLocaleLowerCase());
+        .includes(searchfield.toLocaleLowerCase());
     });
 
-    if(this.state.robots.length === 0) {
-      return <h1>Loading</h1>
-    } else { 
+    if (!robots.length) {
+      return <h1>Loading</h1>;
+    } else {
       return (
         <div className="tc">
           <h1>RoboFriends</h1>
@@ -43,7 +43,6 @@ class App extends Component {
           </Scroll>
         </div>
       );
-
     }
   }
 }
